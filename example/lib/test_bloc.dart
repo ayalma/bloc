082712ae03a6990c9f1f8306afa765ba2b1f947c
Ali Mohammadi <alimohammadi7117@gmail.com>
2019-11-12 15:34:29 +0330

    Fixed example problem

--- example/lib/test_bloc.dart
+++ example/lib/test_bloc.dart
@@ -9,7 +9,7 @@ part 'test_bloc.g.dart';
 @bloc
 class TestBloc extends Bloc with _TestBloc {
   @SinkBind('runTest', '_onAddComment')
-  PublishSubject<String> _addComment;
+  PublishSubject<String> _addComment = PublishSubject();
 
   @BlocStream("result")
   PublishSubject<int> _addCommentState = PublishSubject();
@@ -17,4 +17,12 @@ class TestBloc extends Bloc with _TestBloc {
   _onAddComment(String event) {}
 
   int _i = 0;
+
+  @EventStream(
+    "increment",
+    "incrementResult",
+  )
+  Stream<int> _onRunTest(String event) async* {
+    yield _i++;
+  }
 }
